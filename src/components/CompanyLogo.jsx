17a1ad37328316045f7fae23bef1b0f3d4907b3b
robert-devs <mainrobert04@gmail.com>
2024-03-05 12:08:55 +0300

    adding benefits part

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -3,7 +3,7 @@ import { companyLogos } from "../constants";
 const CompanyLogo = ({ className }) => {
   return (
     <div className={className}>
-      <h5 className="tagline mb-6 text-center text-n-1/50">
+      <h5 className="mb-6 text-center tagline text-n-1/50">
         Helping people to create beautiful content at
       </h5>
       <ul className="flex ">

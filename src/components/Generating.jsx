4dd74dd2476db69a39992b76f68a0e3b7bf9faab
robert-devs <mainrobert04@gmail.com>
2024-03-05 19:38:45 +0300

    updates

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,13 +1,16 @@
-import { loading } from "../assets";
+import { Loader2 } from "lucide-react";
 
 const Generating = ({ className }) => {
   return (
     <div
-      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
+      className={`flex items-center gap-2 h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
         className || ""
       } text-base`}
     >
-      <img src={loading} alt="loading" className="w-5 h-5 mr-4" />
+      <Loader2
+        size={20}
+        className="!font-extrabold text-purple-700 animate-spin"
+      />
       AI is Generating...
     </div>
   );
